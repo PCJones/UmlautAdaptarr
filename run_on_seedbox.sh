eeff05783ea800e2ad9de17666974af82e139cb8
Jonas F <github@pcjones.de>
2025-01-14 01:00:26 +0100

    Create run_on_seedbox.sh

---
+++ run_on_seedbox.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Download linux binary from
+https://github.com/PCJones/UmlautAdaptarr/releases
+
+# script by schumi4 - THX!
+
+#seedbox fix
+export DOTNET_GCHeapHardLimit=20000000
+
+# Basic Configuration
+export TZ=Europe/Berlin
+
+# Sonarr Configuration
+export SONARR__ENABLED=true
+export SONARR__HOST=https://name.server.usbx.me/sonarr/
+export SONARR__APIKEY=APIKEY
+
+# Radarr Configuration
+export RADARR__ENABLED=false
+export RADARR__HOST=http://localhost:7878
+export RADARR__APIKEY=APIKEY
+
+# Readarr Configuration
+export READARR__ENABLED=false
+export READARR__HOST=http://localhost:8787
+export READARR__APIKEY=APIKEY
+
+# Lidarr Configuration
+export LIDARR__ENABLED=false
+export LIDARR__HOST=http://localhost:8686
+export LIDARR__APIKEY=APIKEY
+
+# Multiple Sonarr Instances (commented out by default)
+#export SONARR__0__NAME="NAME 1"
+#export SONARR__0__ENABLED=false
+#export SONARR__0__HOST=http://localhost:8989
+#export SONARR__0__APIKEY=APIKEY
+#export SONARR__1__NAME="NAME 2"
+#export SONARR__1__ENABLED=false
+#export SONARR__1__HOST=http://localhost:8989
+#export SONARR__1__APIKEY=APIKEY
+
+# Advanced Options
+#export IpLeakTest__Enabled=false
+#export SETTINGS__IndexerRequestsCacheDurationInMinutes=12
+export SETTINGS__ApiKey="apikey"
+export SETTINGS__ProxyPort=1234 # Port for Proxy
+export Kestrel__Endpoints__Http__Url="http://[::]:1235" # Port for UmlautAdaptarr API
+
+chmod +x ./publish/UmlautAdaptarr
+
+./publish/UmlautAdaptarr
